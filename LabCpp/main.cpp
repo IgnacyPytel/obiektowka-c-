20d5d142c8d3d6358e0fb2698cfe167071cf574d
ignas int acc <126079737+IgnacyPytel@users.noreply.github.com>
2023-03-10 14:02:55 +0100

    lab02 z obiektowki
    zrobic sprawozdanie do tego

---
+++ LabCpp/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "zlab02.h"
+using namespace std;
+
+int main() {
+    cout << "Laboratorium 02" <<endl;
+    ZLab02 lab02;
+   //lab02.zadanie_2_3();
+   //lab02.zadanie_2_4();
+   //lab02.zadanie_2_5();
+    lab02.menu();
+    return 0;
+}
