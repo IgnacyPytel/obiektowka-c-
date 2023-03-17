e767389c2d9cec0d61790c79085cba5fcf86181c
ignas int acc <126079737+IgnacyPytel@users.noreply.github.com>
2023-03-17 15:37:29 +0100

    cosik naprawione lab3
    bugfix/logika tez naprawiona

---
+++ obiektowka-c--main/LabCpp/obiektowka-c--main/LabCpp/ZLab03.h
@@ -0,0 +1,34 @@
+//
+// Created by student on 17.03.2023.
+//
+
+#ifndef LABCPP_ZLAB03_H
+#define LABCPP_ZLAB03_H
+
+#include <string>
+using namespace std;
+class Prostokat{
+private:
+    string nazwa;
+    double bok1, bok2;
+    bool poprawny;
+    double obwod, pole;
+
+    bool czyPoprawny(double bok1, double bok2);
+    void obliczObwod();
+    void obliczPole();
+
+public:
+    Prostokat(string n = " ? ", double a=1, double b=2);
+    ~Prostokat();
+    bool czyPoprawny();
+    const string& jakaNazwa();
+    double podajPole();
+    double podajObwod();
+    bool zmienBoki(double a, double b);
+    std::string doTekstu();
+};
+
+
+
+#endif //LABCPP_ZLAB03_H
